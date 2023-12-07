3f7eb1529672f583220d1b28b7d3cb8024354526
smanimehalai <manimehalai.s@adlinktech.com>
2023-12-07 16:06:07 +0530

    Updated the paths to edk2_CCoE

--- tools/edk2.sh
+++ tools/edk2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export PACKAGES_PATH=$WORKSPACE/edk2-platforms_CCoE:$WORKSPACE/edk2-platforms_CCoE/Features/Intel:$WORKSPACE/edk2
+export PACKAGES_PATH=$WORKSPACE/edk2-platforms_CCoE:$WORKSPACE/edk2-platforms_CCoE/Features/Intel:$WORKSPACE/edk2_CCoE
 export PYTHON_COMMAND=/usr/bin/python3.6
 # export CROSS_COMPILE=aarch64-ampere-linux-gnu-
-. $WORKSPACE/edk2/edksetup.sh
+. $WORKSPACE/edk2_CCoE/edksetup.sh
